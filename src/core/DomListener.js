f002e7993eeece22d30d8c60a76e46b8118d6e03
Zhigulskiy <evgeniy1st@users.noreply.github.com>
2021-01-06 01:10:34 +0300

    create base structure

---
+++ src/core/DomListener.js
@@ -0,0 +1,34 @@
+import {capitalize} from '@core/utils';
+
+export class DomListener {
+  constructor(root, options = {}) {
+    if (!root) {
+      throw new Error(`No $root provided for DomListener`);
+    }
+    this.$root = root;
+    this.listeners = options.listeners || [];
+    this.name = options.name || '';
+  }
+
+  initDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        throw new Error(`${method} is not implemented in ${this.name}`);
+      }
+      this[method] = this[method].bind(this);
+      this.$root.on(listener, this[method]);
+    });
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      this.$root.off(listener, this[method]);
+    });
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
+}
